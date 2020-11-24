001917a43415c09fc3020fe619603797865ac3ee
expressgh <expressgh@protonmail.com>
2020-11-24 15:45:49 +0100

    Hack The Future 2020

--- TheFellowshipOfCode.DotNet/TheFellowshipOfCode.DotNet.YourAdventure/MyAdventure.cs
+++ TheFellowshipOfCode.DotNet/TheFellowshipOfCode.DotNet.YourAdventure/MyAdventure.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
+using System.Net.Security;
 using System.Threading.Tasks;
 using HTF2020.Contracts;
 using HTF2020.Contracts.Enums;
 using HTF2020.Contracts.Models;
 using HTF2020.Contracts.Models.Adventurers;
+using HTF2020.Contracts.Models.Enemies;
+using HTF2020.Contracts.Models.Party;
 using HTF2020.Contracts.Requests;
+using HTF2020.GameController.State;
+
 
 namespace TheFellowshipOfCode.DotNet.YourAdventure
 {
@@ -43,13 +49,16 @@ namespace TheFellowshipOfCode.DotNet.YourAdventure
 
             Task<Turn> PlayToEnd()
             {
-                return Task.FromResult(request.PossibleActions.Contains(TurnAction.WalkSouth) ? new Turn(TurnAction.WalkSouth) : new Turn(request.PossibleActions[_random.Next(request.PossibleActions.Length)]));
+                return Task.FromResult(request.PossibleActions.Contains(TurnAction.WalkEast)
+                    ? new Turn(TurnAction.WalkEast)
+                    : new Turn(request.PossibleActions[_random.Next(request.PossibleActions.Length)]));
             }
 
             Task<Turn> Strategic()
             {
                 const double goingEastBias = 0.35;
                 const double goingSouthBias = 0.25;
+
                 if (request.PossibleActions.Contains(TurnAction.Loot))
                 {
                     return Task.FromResult(new Turn(TurnAction.Loot));
@@ -65,12 +74,40 @@ namespace TheFellowshipOfCode.DotNet.YourAdventure
                     return Task.FromResult(new Turn(TurnAction.WalkEast));
                 }
 
-                if (request.PossibleActions.Contains(TurnAction.WalkSouth) && _random.NextDouble() > (1 - goingSouthBias))
+                if (request.PossibleActions.Contains(TurnAction.WalkSouth) &&
+                    _random.NextDouble() > (1 - goingSouthBias))
                 {
                     return Task.FromResult(new Turn(TurnAction.WalkSouth));
                 }
 
                 return Task.FromResult(new Turn(request.PossibleActions[_random.Next(request.PossibleActions.Length)]));
+            }
+
+            void Main(string[] args)
+            {
+                var map = request.Map;
+                var start = new Tile();
+                var finish = new Tile();
+
+                for (var j = 0; j >= map.Tiles.Rank; j++)
+                {
+                    for (var i = 0; i >= map.Tiles.Col; i++)
+                    {
+                        if (map.Tiles[j, i] == "A")
+                        {
+                            start = map.Tiles[j, i];
+                        }
+
+                        if (map.Tiles[j, i] == "F")
+                        {
+                            finish = map.Tiles[j, i];
+                        }
+                    }
+                }
+
+                var activeTiles = new List<Tile>();
+                activeTiles.Add(start);
+                var visitedTiles = new List<Tile>();
             }
         }
     }
